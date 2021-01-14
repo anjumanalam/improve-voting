d9f9ae7772be1b6f4bf51763b57790de6898a153
anjumanalam <aaalam@syr.edu>
2021-01-13 22:50:36 -0500

    Add project R file

---
+++ final-project.R
@@ -0,0 +1,41 @@
+################################################
+# IST687, Standard Homework Heading
+#
+# Student name: Anjuman Alam
+# Final Project
+# Date due: January 17, 2021
+#
+# Attribution statement:
+# 2. I did this project with help from the book and the professor and these Internet sources:
+#
+
+dev.off() # Clear the graph window
+cat('\014')   # Clear the console
+rm(list=ls()) # Clear user objects from the environment
+
+# import packages
+library(tidyverse)
+library(ggplot2)
+library(kernlab)
+library(arules)
+library(e1071)
+library(arulesViz)
+
+# User-defined functions
+
+
+
+#import data-set of results from poll on why Americans don't vote
+
+
+#
+
+
+
+
+
+
+
+
+
+
